e432f0eea1dc9af16d281ab045ecec2d4dd50210
Phong <phongham2410@gmail.com>
2025-04-03 21:54:40 +0700

    xoa-nhung-file thua

--- src/app/middleware/common/verify-token.js
+++ src/app/middleware/common/verify-token.js
@@ -4,7 +4,7 @@ import {Admin, Role, User} from "@/app/models";
 import {tokenBlocklist} from "@/app/services/auth.service";
 import {ACCOUNT_TYPE, SECRET_KEY, STATUS_ACTIVE, TOKEN_TYPE} from "@/configs";
 import {responseError, getToken} from "@/utils/helpers";
-import { Permission } from "@/app/models/permission";
+import {Permission} from "@/app/models/permission";
 
 export async function verifyToken(req, res, next) {
     try {
@@ -18,16 +18,26 @@ export async function verifyToken(req, res, next) {
                 if (type === TOKEN_TYPE.AUTHORIZATION) {
                     let account;
                     let permissions;
-                    switch(data.account_type){
+                    switch (data.account_type) {
                         case ACCOUNT_TYPE.USER:
-                            account = await User.findOne({_id: data.account_id, deleted: false, status: STATUS_ACTIVE.ACTIVE});
+                            account = await User.findOne({
+                                _id: data.account_id,
+                                deleted: false,
+                                status: STATUS_ACTIVE.ACTIVE,
+                            });
                             break;
                         case ACCOUNT_TYPE.ADMIN:
-                            account = await Admin.findOne({_id: data.account_id, deleted: false, status: STATUS_ACTIVE.ACTIVE});
+                            account = await Admin.findOne({
+                                _id: data.account_id,
+                                deleted: false,
+                                status: STATUS_ACTIVE.ACTIVE,
+                            });
                             if (account) {
-                                const roles = await Role.find({ _id: { $in: account.role_ids }, deleted: false });
-                                permissions = await Permission.find({ _id: { $in: roles.map(role => role.permission_ids).flat() } });
-                                req.permissions =  permissions.map(item => item.code);
+                                const roles = await Role.find({_id: {$in: account.role_ids}, deleted: false});
+                                permissions = await Permission.find({
+                                    _id: {$in: roles.map((role) => role.permission_ids).flat()},
+                                });
+                                req.permissions = permissions.map((item) => item.code);
                             }
                             break;
                     }
@@ -39,7 +49,11 @@ export async function verifyToken(req, res, next) {
                 }
 
                 if (type === TOKEN_TYPE.FORGOT_PASSWORD) {
-                    const account = await Admin.findOne({_id: data.account_id, deleted: false, status: STATUS_ACTIVE.ACTIVE});
+                    const account = await Admin.findOne({
+                        _id: data.account_id,
+                        deleted: false,
+                        status: STATUS_ACTIVE.ACTIVE,
+                    });
 
                     if (account) {
                         account.account_type = data.account_type;

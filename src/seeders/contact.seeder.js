7f7a875094260fbd82503ecd7fc0a5bc113d684f
Phong <phongham2410@gmail.com>
2025-04-05 11:34:45 +0700

    ban giao code cho canh

--- src/seeders/contact.seeder.js
+++ src/seeders/contact.seeder.js
@@ -12,39 +12,39 @@ async function copyImages() {
         fs.mkdirSync(uploadDir);
     }
 
-    images.forEach(image => {
+    images.forEach((image) => {
         const srcPath = path.join(sampleDir, image);
         const destPath = path.join(uploadDir, image);
 
         fs.copyFileSync(srcPath, destPath);
     });
 
-    return images.map(image => path.join("uploads", "images", image));
+    return images.map((image) => path.join("uploads", "images", image));
 }
 
-export default async function() {
+export default async function () {
     const imagePaths = await copyImages();
 
     const contact = {
         type: CONFIG_TYPE.CONTACT,
         config: {
-            phone: "0333402699",
-            email: "dangvanhoang@gmail.com",
+            phone: "0362800999",
+            email: "duccanh@gmail.com",
             socials: [
                 {
                     icon: imagePaths[0] || "",
-                    link: "#"
+                    link: "#",
                 },
                 {
                     icon: imagePaths[1] || "",
-                    link: "#"
+                    link: "#",
                 },
                 {
                     icon: imagePaths[2] || "",
-                    link: "#"
+                    link: "#",
                 },
-            ]
-        }
+            ],
+        },
     };
 
     await Config.findOneAndUpdate({type: contact.type}, {$set: contact}, {upsert: true});
--- src/seeders/user-feedback.seeder.js
+++ src/seeders/user-feedback.seeder.js
@@ -5,20 +5,23 @@ export default async function userFeedbackSeeder() {
         {
             avatar: "avatar-0.png",
             cover: "cover-0.jpeg",
-            name: "Nguyễn Văn Phong",
-            content: "<p>Tôi rất hài lòng với khóa tập của trung tâm, hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng  </p>",
+            name: "Nguyễn Đức Cảnh",
+            content:
+                "<p>Phòng tập rất sạch sẽ và đầy đủ thiết bị. Huấn luyện viên ở đây cực kỳ nhiệt tình và chuyên nghiệp. Mình cảm thấy rất có động lực mỗi khi đến đây tập luyện!</p>",
         },
         {
             avatar: "avatar-1.jpeg",
             cover: "cover-1.jpeg",
-            name: "Đặng Văn Hoàng",
-            content: "<p>Tôi rất hài lòng với khóa tập của trung tâm, hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng  </p>",
+            name: "Nguyễn Văn Chiến",
+            content:
+                "<p>Không gian tập rộng rãi, thoáng mát. Mình đặc biệt thích lớp yoga buổi sáng – vừa thư giãn, vừa giúp cải thiện sức khỏe rõ rệt sau vài tuần tập</p>",
         },
         {
             avatar: "avatar-2.png",
             cover: "cover-2.jpeg",
-            name: "Nguyễn Phúc Đức",
-            content: "<p>Tôi rất hài lòng với khóa tập của trung tâm, hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng hài lòng  </p>",
+            name: "Nguyễn Văn Phong",
+            content:
+                "<p>Dịch vụ khách hàng ở đây rất tốt. Mỗi khi có thắc mắc hoặc cần hỗ trợ, nhân viên luôn sẵn sàng giúp đỡ. Rất hài lòng và sẽ tiếp tục gắn bó lâu dài.</p>",
         },
     ];
 
